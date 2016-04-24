dd1993560ca6f510364f5d76300045fd62f66499
Bruno Marques <bomarques@inf.ufrgs.br>
2016-04-24 18:31:50 -0300

    Parallel data loading

--- loaddata.py
+++ loaddata.py
@@ -4,6 +4,7 @@ import os
 
 import lxml.html
 import elasticsearch
+import elasticsearch.helpers as es_helpers
 
 DB_INDEX = 'efe95'
 
@@ -33,12 +34,23 @@ es.indices.put_mapping('doc', {
     }
 }, DB_INDEX)
 
+
+def actions(docs):
+    for doc in docs:
+        body = {child.tag: child.text for child in doc.iterchildren()}
+        yield {
+            '_op_type': 'create',
+            '_index': DB_INDEX,
+            '_type': 'doc',
+            '_id': body['docid'],
+            '_source': body,
+        }
+
 for filename in sorted(os.listdir('efe95')):
     print('Loading ', filename)
     with open('efe95/{}'.format(filename), encoding='iso-8859-1') as data:
         docs = lxml.html.fragments_fromstring(data.read())
     print('Loaded ', filename)
-    for doc in docs:
-        body = {child.tag: child.text for child in doc.iterchildren()}
-        es.create(DB_INDEX, 'doc', body, id=body['docid'], ignore=409)
+    for success, info in es_helpers.parallel_bulk(es, actions(docs)):
+        pass
     print('Finished writing documents from ', filename)
